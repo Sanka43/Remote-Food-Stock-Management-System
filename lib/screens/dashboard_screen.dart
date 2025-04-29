a1fe2b4160882c16706b8afff893dfa17df6a1ff
Sanka43 <git@github.com>
2025-04-29 17:37:02 +0530

    log in

---
+++ lib/screens/dashboard_screen.dart
@@ -0,0 +1,282 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> stockItems = [
+    {"name": "Tomatoes", "stock": 80, "maxStock": 100},
+    {"name": "Potatoes", "stock": 50, "maxStock": 100},
+    {"name": "Onions", "stock": 30, "maxStock": 100},
+    {
+      "name": "Milk",
+      "stock": 2,
+      "maxStock": 50
+    }, // LOW STOCK (Shows Order Button)
+    {
+      "name": "Eggs",
+      "stock": 5,
+      "maxStock": 30
+    }, // LOW STOCK (Shows Order Button)
+  ];
+
+  DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.green),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(Icons.account_circle, size: 60, color: Colors.white),
+                  SizedBox(height: 10),
+                  Text("Welcome, User!",
+                      style: TextStyle(fontSize: 18, color: Colors.white)),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.kitchen),
+              title: Text("Kitchen Inventory"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Settings"),
+              onTap: () {},
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.logout, color: Colors.red),
+              title: Text("Logout", style: TextStyle(color: Colors.red)),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pop(context); // Go back to Login Screen
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        color: const Color.fromARGB(
+            255, 226, 226, 226), // Set the solid color background (blue here)
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Dashboard Cards (Horizontal)
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      _buildDashboardCard(
+                          "Products", Icons.inventory, Colors.blue),
+                      _buildDashboardCard(
+                          "Categories", Icons.category, Colors.orange),
+                      _buildDashboardCard(
+                          "Suppliers", Icons.local_shipping, Colors.purple),
+                      _buildDashboardCard(
+                          "Reports", Icons.analytics, Colors.teal),
+                      _buildDashboardCard(
+                          "Orders", Icons.shopping_cart, Colors.red),
+                    ],
+                  ),
+                ),
+              ),
+
+              // Stock Levels Title
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                child: Text(
+                  "Stock Levels",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+
+              // Stock Levels (Horizontal Scroll)
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  children: stockItems
+                      .map((item) => _buildStockItem(context, item))
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Widget for Dashboard Cards
+  Widget _buildDashboardCard(String title, IconData icon, Color color) {
+    return Container(
+      width: 150,
+      margin: EdgeInsets.only(right: 10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20), // Rounding corners
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.2), // Transparent background
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 1,
+                blurRadius: 8,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
+          child: InkWell(
+            onTap: () {},
+            borderRadius: BorderRadius.circular(20),
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(icon, size: 50, color: color),
+                  SizedBox(height: 10),
+                  Text(title,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Widget for Stock Levels (Horizontal Cards)
+  Widget _buildStockItem(BuildContext context, Map<String, dynamic> item) {
+    double percentage = item["stock"] / item["maxStock"];
+    Color progressColor = percentage > 0.5
+        ? Colors.green
+        : (percentage > 0.2 ? Colors.orange : Colors.red);
+
+    return Container(
+      width: 150,
+      height: 400,
+      margin: EdgeInsets.only(right: 10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.5), // Transparent background
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 1,
+                blurRadius: 8,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(12),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(item["name"],
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+                SizedBox(height: 5),
+                Container(
+                  width: 20,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  alignment: Alignment.bottomCenter,
+                  child: FractionallySizedBox(
+                    heightFactor: percentage,
+                    child: Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: progressColor,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(" ${item["stock"]} / ${item["maxStock"]}",
+                    style: TextStyle(color: Colors.white)),
+                if (percentage < 0.2)
+                  Padding(
+                    padding: EdgeInsets.only(top: 8),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        _showOrderDialog(context, item["name"]);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                      ),
+                      child: Text("Order Now"),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Show Order Dialog
+  void _showOrderDialog(BuildContext context, String itemName) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Order Item"),
+          content: Text("Do you want to order more $itemName?"),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text("Cancel", style: TextStyle(color: Colors.red)),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("$itemName order placed!")));
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              child: Text("Confirm"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,263 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dashboard_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool _rememberMe = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSavedCredentials();
+  }
+
+  // Load saved credentials
+  Future<void> _loadSavedCredentials() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _usernameController.text = prefs.getString('username') ?? "";
+      _passwordController.text = prefs.getString('password') ?? "";
+      _rememberMe = prefs.getBool('rememberMe') ?? false;
+    });
+  }
+
+  // Save credentials
+  Future<void> _saveCredentials() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (_rememberMe) {
+      await prefs.setString('username', _usernameController.text);
+      await prefs.setString('password', _passwordController.text);
+      await prefs.setBool('rememberMe', true);
+    } else {
+      await prefs.remove('username');
+      await prefs.remove('password');
+      await prefs.setBool('rememberMe', false);
+    }
+  }
+
+  // Show login popup
+  void _showLoginPopup(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.transparent,
+          child: Stack(
+            children: [
+              // Blur Effect
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+                child: Container(color: Colors.transparent),
+              ),
+              // Login Box
+              Container(
+                padding: EdgeInsets.all(0),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  // mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Smart Kitchen World",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white)),
+                    SizedBox(height: 20),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: _usernameController,
+                            decoration: InputDecoration(
+                              labelText: "Username",
+                              labelStyle: TextStyle(
+                                color: Colors.white,
+                              ),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              filled: true,
+                              fillColor: Colors.white.withOpacity(0.3),
+                            ),
+                            style: TextStyle(color: Colors.white),
+                            validator: (value) =>
+                                value == "demo" ? null : "Invalid username",
+                          ),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: _passwordController,
+                            decoration: InputDecoration(
+                              labelText: "Password",
+                              labelStyle: TextStyle(color: Colors.white),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              filled: true,
+                              fillColor: Colors.white.withOpacity(0.3),
+                            ),
+                            obscureText: true,
+                            style: TextStyle(color: Colors.white),
+                            validator: (value) =>
+                                value == "demo1234" ? null : "Invalid password",
+                          ),
+                          SizedBox(height: 10),
+                          // Remember Me Checkbox
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Checkbox(
+                                    value: _rememberMe,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _rememberMe = value!;
+                                      });
+                                    },
+                                  ),
+                                  Text("Remember Me",
+                                      style: TextStyle(
+                                        color: Colors.white, 
+                                        fontSize: 12)),
+                                ],
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                child: Text("Forgot Password?",
+                                    style: TextStyle(color: Colors.white,fontSize: 12)),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          // Login Button
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 50, vertical: 10),
+                              backgroundColor:
+                                  const Color.fromARGB(96, 17, 117, 4)
+                                      .withOpacity(0.7),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50)),
+                            ),
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                _saveCredentials();
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            DashboardScreen()));
+                              }
+                            },
+                            child: Text("Login",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.white,
+                                  shadows: [
+                                    Shadow(
+                                        color:
+                                            const Color.fromARGB(255, 0, 0, 0),
+                                        offset: Offset(2, 2),
+                                        blurRadius: 5),
+                                  ],
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // Background Image with Gradient
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/background.jpg"),
+                fit: BoxFit.cover,
+              ),
+              gradient: LinearGradient(
+                colors: [Colors.black.withOpacity(0.6), Colors.transparent],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          // Center Content
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // App Title
+              Text(
+                "Smart Kitchen\nWorld", // add \n to break line
+                textAlign: TextAlign.center, // Ensures text is centered
+                style: TextStyle(
+                  fontFamily: "cursive",
+                  fontSize: 56,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  shadows: [
+                    Shadow(
+                        color: Colors.black87,
+                        offset: Offset(5, 5),
+                        blurRadius: 5),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20), // Space before button
+
+              // Login Button
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  backgroundColor:
+                      const Color.fromARGB(255, 0, 0, 0).withOpacity(0.7),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                ),
+                onPressed: () => _showLoginPopup(context),
+                child: Text("Login",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      shadows: [
+                        Shadow(
+                            color: Colors.black87,
+                            offset: Offset(2, 2),
+                            blurRadius: 5),
+                      ],
+                    )),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
